ccf1392416c23e1cc843309b634f9c92d4a1506a
OmarVoltex-M-O <OmarVoltex.M.O@outlook.com>
2026-09-08 16:17:43 +0300

    Added some helper macroes and disabled the copying of VX_Object class and exported the VX_Object Class

---
+++ VoltexGUI/Src/Common/HelperMacroes/VX_HelperMacroes.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define VX_DISABLE_CLASS_COPYING(Class) \
+	Class(const Class&) = delete; \
+	Class& operator=(const Class&) = delete;
+
+#define VX_DISABLE_CLASS_MOVING(Class) \
+	Class(Class&&) = delete; \
+	Class& operator=(Class&&) = delete;
+
+#define VX_DISABLE_CLASS_COPYING_MOVING(Class) \
+	VX_DISABLE_CLASS_COPYING(Class) \
+	VX_DISABLE_CLASS_MOVING(Class) \
+
+#define VX_DECLARE_PRIVATE(Class) \
+	inline Class##Private* d_func() noexcept { return reinterpret_cast<Class##Private*>(d_ptr); } \
+	inline [[nodiscard]]const Class##Private* d_func() const noexcept { return reinterpret_cast<const Class##Private*>(d_ptr); } \
+	friend Class##Private;
+
+#define VX_DECLARE_PUBLIC(Class) \
+	inline Class* q_func() noexcept { return reinterpret_cast<Class*>(q_ptr); } \
+	inline [[nodiscard]]const Class* q_func() const noexcept { return reinterpret_cast<const Class*>(q_ptr); } \
+	friend Class;
+
+#define VX_D(Class) Class##Private* d = d_func(); 
+#define VX_Q(Class) Class* q = q_func(); 
--- VoltexGUI/Src/Common/Inc/VX_Object.h
+++ VoltexGUI/Src/Common/Inc/VX_Object.h
@@ -1,11 +1,16 @@
 #pragma once
+#include "VX_DLLExport.h"
+#include "VX_HelperMacroes.h"
 
 namespace Voltex {
 	class VX_ObjectPrivate;
-	class VX_Object {
+	class VX_API VX_Object {
 	public:
 		VX_Object(VX_ObjectPrivate& dd);
 		~VX_Object();
 		VX_ObjectPrivate* d_ptr;
+
+	private:
+		VX_DISABLE_CLASS_COPYING(VX_Object);
 	};
 }
